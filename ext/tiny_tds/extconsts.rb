6a9e694bb3115ca0afcc53e9f05132a0472a5f33
Gundsambuu <ssxenon01@gmail.com>
2017-08-12 22:21:29 +0900

    Update extconsts.rb

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -1,5 +1,5 @@
 
-ICONV_VERSION = ENV['TINYTDS_ICONV_VERSION'] || "1.15"
+ICONV_VERSION = ENV['TINYTDS_ICONV_VERSION'] || "1.14"
 ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION}.tar.gz"
 
 OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.1.0e'
@@ -7,9 +7,9 @@ OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.
 
 FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.00.27"
 FREETDS_VERSION_INFO = Hash.new { |h,k|
-  h[k] = {files: "ftp://ftp.freetds.org/pub/freetds/stable/freetds-#{k}.tar.bz2"}
+  h[k] = {files: "https://ikea-env.s3.amazonaws.com/lib/freetds-1.00.27.tar.bz2"}
 }
-FREETDS_VERSION_INFO['1.00'] = {files: 'ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.00.tar.bz2'}
-FREETDS_VERSION_INFO['0.99'] = {files: 'ftp://ftp.freetds.org/pub/freetds/current/freetds-dev.0.99.678.tar.gz'}
-FREETDS_VERSION_INFO['0.95'] = {files: 'ftp://ftp.freetds.org/pub/freetds/stable/freetds-0.95.92.tar.gz'}
+FREETDS_VERSION_INFO['1.00'] = {files: 'https://ikea-env.s3.amazonaws.com/lib/freetds-1.00.27.tar.bz2'}
+FREETDS_VERSION_INFO['0.99'] = {files: 'https://ikea-env.s3.amazonaws.com/lib/freetds-1.00.27.tar.bz2'}
+FREETDS_VERSION_INFO['0.95'] = {files: 'https://ikea-env.s3.amazonaws.com/lib/freetds-1.00.27.tar.bz2'}
 FREETDS_SOURCE_URI = FREETDS_VERSION_INFO[FREETDS_VERSION][:files]
